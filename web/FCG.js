4aea89c1d7adf3f960b21e2351de34827efc8b78
Martín Lombardo <to@mlomb.me>
2021-11-25 19:48:39 -0300

    ui debugging

--- web/FCG.js
+++ web/FCG.js
@@ -1,5 +1,17 @@
 
+const canvas = document.getElementById('canvas');
+const gui = new dat.GUI({name: 'My GUI'});
 
+const terrain_params = {
+    param1: 42,
+    param2: 69,
+};
+
+gui.add(terrain_params, 'param1', 0, 100);
+gui.add(terrain_params, 'param2', 0, 100);
+
+
+console.log(canvas);
 
 Module.onRuntimeInitialized = () => {
     
