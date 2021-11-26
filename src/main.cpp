dd7327eb8b91ec39006112fc8fdc784907be2c12
Martín Lombardo <to@mlomb.me>
2021-11-26 01:58:31 -0300

    funcionando pero ahora bien

--- src/main.cpp
+++ src/main.cpp
@@ -6,169 +6,179 @@
 using namespace std;
 
 struct Vertex {
-	float x;
-	float y;
-	float z;
+    float x;
+    float y;
+    float z;
 };
 
 Vertex operator-(const Vertex& a, const Vertex& b) {
-	Vertex c;
-	c.x = a.x - b.x;
-	c.y = a.y - b.y;
-	c.z = a.z - b.z;
-	return c;
+    Vertex c;
+    c.x = a.x - b.x;
+    c.y = a.y - b.y;
+    c.z = a.z - b.z;
+    return c;
+}
+
+Vertex operator*(const Vertex& a, const float& b) {
+    return { a.x * b, a.y * b, a.z * b };
 }
 
 Vertex crossProduct(const Vertex& A, const Vertex& B) {
-	Vertex C;
-	C.x = A.y * B.z - A.z * B.y;
-	C.y = A.z * B.x - A.x * B.z;
-	C.z = A.x * B.y - A.y * B.x;
-	return C;
+    Vertex C;
+    C.x = A.y * B.z - A.z * B.y;
+    C.y = A.z * B.x - A.x * B.z;
+    C.z = A.x * B.y - A.y * B.x;
+    return C;
 }
 
 Vertex normalize(const Vertex& V) {
-	float length = sqrt(V.x * V.x + V.y * V.y + V.z * V.z);
-	Vertex N;
-	N.x = V.x / length;
-	N.y = V.y / length;
-	N.z = V.z / length;
-	return N;
+    float length = sqrt(V.x * V.x + V.y * V.y + V.z * V.z);
+    Vertex N;
+    N.x = V.x / length;
+    N.y = V.y / length;
+    N.z = V.z / length;
+    return N;
 }
 
 inline Vertex vertexInterp(const float isolevel, const Vertex& p1, const  Vertex& p2, const float v1, const float v2) {
-	if (abs(isolevel - v1) < 0.00001 || abs(v1 - v2) < 0.00001)
-		return(p1);
-	if (abs(isolevel - v2) < 0.00001)
-		return(p2);
+    if (abs(isolevel - v1) < 0.00001 || abs(v1 - v2) < 0.00001)
+        return(p1);
+    if (abs(isolevel - v2) < 0.00001)
+        return(p2);
 
-	float mu = (isolevel - v1) / (v2 - v1);
-	return {
-		p1.x + mu * (p2.x - p1.x),
-		p1.y + mu * (p2.y - p1.y),
-		p1.z + mu * (p2.z - p1.z)
-	};
+    float mu = (isolevel - v1) / (v2 - v1);
+    return {
+        p1.x + mu * (p2.x - p1.x),
+        p1.y + mu * (p2.y - p1.y),
+        p1.z + mu * (p2.z - p1.z)
+    };
 }
 
 extern "C" {
-	int generate_mesh(Vertex* vertArray, Vertex* normArray, const int SIZE, const float isolevel) {
-		FastNoiseLite noise;
-		noise.SetNoiseType(FastNoiseLite::NoiseType::NoiseType_Perlin);
+    int generate_mesh(Vertex* vertArray, Vertex* normArray, const int SIZE, const float isolevel) {
+        FastNoiseLite noise;
+        noise.SetNoiseType(FastNoiseLite::NoiseType::NoiseType_Perlin);
 
-		const int CELLS = SIZE * SIZE * SIZE;
-		float* points = (float*)malloc(CELLS * sizeof(float));
+        const int CELLS = SIZE * SIZE * SIZE;
+        float* points = (float*)malloc(CELLS * sizeof(float));
 
-		// 1. Generar la nube de puntos
-		for (int x = 0; x < SIZE; x++) {
-			for (int y = 0; y < SIZE; y++) {
-				for (int z = 0; z < SIZE; z++) {
-					float val = (noise.GetNoise((float)x, (float)y, (float)z) + 1.0f) / 2.0f;
+        // 1. Generar la nube de puntos
+        for (int x = 0; x < SIZE; x++) {
+            for (int y = 0; y < SIZE; y++) {
+                for (int z = 0; z < SIZE; z++) {
+                    float val = (noise.GetNoise((float)x, (float)y, (float)z) + 1.0f) / 2.0f;
 
-					points[x * SIZE * SIZE + y * SIZE + z] = val;
-				}
-			}
-		}
+                    points[x * SIZE * SIZE + y * SIZE + z] = val;
+                }
+            }
+        }
 
-		int numVertex = 0;
-		float val[8];
-		Vertex p[8];
-		Vertex vertexList[12];
+        int numVertex = 0;
+        float val[8];
+        Vertex p[8];
+        Vertex vertexList[12];
 
-		// 2. Marching Cubes
-		for (int x = 0; x < SIZE - 1; x++) {
-			for (int y = 0; y < SIZE - 1; y++) {
-				for (int z = 0; z < SIZE - 1; z++) {
-					float xx = x;
-					float yy = y;
-					float zz = z;
-					p[0] = { xx, yy, zz };
-					p[1] = { xx + 1, yy, zz };
-					p[2] = { xx + 1,yy,zz + 1 };
-					p[3] = { xx, yy, zz + 1 };
-					p[4] = { xx, yy + 1, zz };
-					p[5] = { xx + 1, yy + 1, zz };
-					p[6] = { xx + 1, yy + 1, zz + 1 };
-					p[7] = { xx , yy + 1, zz + 1 };
+        // 2. Marching Cubes
+        for (int x = 0; x < SIZE - 1; x++) {
+            for (int y = 0; y < SIZE - 1; y++) {
+                for (int z = 0; z < SIZE - 1; z++) {
+                    float xx = x;
+                    float yy = y;
+                    float zz = z;
+                    p[0] = { xx, yy, zz };
+                    p[1] = { xx + 1, yy, zz };
+                    p[2] = { xx + 1,yy,zz + 1 };
+                    p[3] = { xx, yy, zz + 1 };
+                    p[4] = { xx, yy + 1, zz };
+                    p[5] = { xx + 1, yy + 1, zz };
+                    p[6] = { xx + 1, yy + 1, zz + 1 };
+                    p[7] = { xx , yy + 1, zz + 1 };
 
-					int cubeIndex = 0;
-					val[0] = points[(x + 0) * SIZE * SIZE + (y + 0) * SIZE + (z + 0)];
-					val[1] = points[(x + 1) * SIZE * SIZE + (y + 0) * SIZE + (z + 0)];
-					val[2] = points[(x + 1) * SIZE * SIZE + (y + 0) * SIZE + (z + 1)];
-					val[3] = points[(x + 0) * SIZE * SIZE + (y + 0) * SIZE + (z + 1)];
-					val[4] = points[(x + 0) * SIZE * SIZE + (y + 1) * SIZE + (z + 0)];
-					val[5] = points[(x + 1) * SIZE * SIZE + (y + 1) * SIZE + (z + 0)];
-					val[6] = points[(x + 1) * SIZE * SIZE + (y + 1) * SIZE + (z + 1)];
-					val[7] = points[(x + 0) * SIZE * SIZE + (y + 1) * SIZE + (z + 1)];
+                    int cubeIndex = 0;
+                    val[0] = points[(x + 0) * SIZE * SIZE + (y + 0) * SIZE + (z + 0)];
+                    val[1] = points[(x + 1) * SIZE * SIZE + (y + 0) * SIZE + (z + 0)];
+                    val[2] = points[(x + 1) * SIZE * SIZE + (y + 0) * SIZE + (z + 1)];
+                    val[3] = points[(x + 0) * SIZE * SIZE + (y + 0) * SIZE + (z + 1)];
+                    val[4] = points[(x + 0) * SIZE * SIZE + (y + 1) * SIZE + (z + 0)];
+                    val[5] = points[(x + 1) * SIZE * SIZE + (y + 1) * SIZE + (z + 0)];
+                    val[6] = points[(x + 1) * SIZE * SIZE + (y + 1) * SIZE + (z + 1)];
+                    val[7] = points[(x + 0) * SIZE * SIZE + (y + 1) * SIZE + (z + 1)];
 
-					// Busco qué vertices están dentro del cubo
-					// y determino el índice en edgeTable
-					if (val[0] < isolevel) cubeIndex |= 1;
-					if (val[1] < isolevel) cubeIndex |= 2;
-					if (val[2] < isolevel) cubeIndex |= 4;
-					if (val[3] < isolevel) cubeIndex |= 8;
-					if (val[4] < isolevel) cubeIndex |= 16;
-					if (val[5] < isolevel) cubeIndex |= 32;
-					if (val[6] < isolevel) cubeIndex |= 64;
-					if (val[7] < isolevel) cubeIndex |= 128;
+                    // Busco qué vertices están dentro del cubo
+                    // y determino el índice en edgeTable
+                    if (val[0] < isolevel) cubeIndex |= 1;
+                    if (val[1] < isolevel) cubeIndex |= 2;
+                    if (val[2] < isolevel) cubeIndex |= 4;
+                    if (val[3] < isolevel) cubeIndex |= 8;
+                    if (val[4] < isolevel) cubeIndex |= 16;
+                    if (val[5] < isolevel) cubeIndex |= 32;
+                    if (val[6] < isolevel) cubeIndex |= 64;
+                    if (val[7] < isolevel) cubeIndex |= 128;
 
-					// Si no hay ningún vertice dentro del cubo, no hago nada
-					if (edgeTable[cubeIndex] == 0) continue;
+                    // Si no hay ningún vertice dentro del cubo, no hago nada
+                    if (edgeTable[cubeIndex] == 0) continue;
 
-					// Si hay alguno, busco los triangulos
-					int edge = edgeTable[cubeIndex];
+                    // Si hay alguno, busco los triangulos
+                    int edge = edgeTable[cubeIndex];
 
-					if (edge & 1)
-						vertexList[0] = vertexInterp(isolevel, p[0], p[1], val[0], val[1]);
-					if (edge & 2)
-						vertexList[1] = vertexInterp(isolevel, p[1], p[2], val[1], val[2]);
-					if (edge & 4)
-						vertexList[2] = vertexInterp(isolevel, p[2], p[3], val[2], val[3]);
-					if (edge & 8)
-						vertexList[3] = vertexInterp(isolevel, p[3], p[0], val[3], val[0]);
-					if (edge & 16)
-						vertexList[4] = vertexInterp(isolevel, p[4], p[5], val[4], val[5]);
-					if (edge & 32)
-						vertexList[5] = vertexInterp(isolevel, p[5], p[6], val[5], val[6]);
-					if (edge & 64)
-						vertexList[6] = vertexInterp(isolevel, p[6], p[7], val[6], val[7]);
-					if (edge & 128)
-						vertexList[7] = vertexInterp(isolevel, p[7], p[4], val[7], val[4]);
-					if (edge & 256)
-						vertexList[8] = vertexInterp(isolevel, p[0], p[4], val[0], val[4]);
-					if (edge & 512)
-						vertexList[9] = vertexInterp(isolevel, p[1], p[5], val[1], val[5]);
-					if (edge & 1024)
-						vertexList[10] = vertexInterp(isolevel, p[2], p[6], val[2], val[6]);
-					if (edge & 2048)
-						vertexList[11] = vertexInterp(isolevel, p[3], p[7], val[3], val[7]);
+                    if (edge & 1)
+                        vertexList[0] = vertexInterp(isolevel, p[0], p[1], val[0], val[1]);
+                    if (edge & 2)
+                        vertexList[1] = vertexInterp(isolevel, p[1], p[2], val[1], val[2]);
+                    if (edge & 4)
+                        vertexList[2] = vertexInterp(isolevel, p[2], p[3], val[2], val[3]);
+                    if (edge & 8)
+                        vertexList[3] = vertexInterp(isolevel, p[3], p[0], val[3], val[0]);
+                    if (edge & 16)
+                        vertexList[4] = vertexInterp(isolevel, p[4], p[5], val[4], val[5]);
+                    if (edge & 32)
+                        vertexList[5] = vertexInterp(isolevel, p[5], p[6], val[5], val[6]);
+                    if (edge & 64)
+                        vertexList[6] = vertexInterp(isolevel, p[6], p[7], val[6], val[7]);
+                    if (edge & 128)
+                        vertexList[7] = vertexInterp(isolevel, p[7], p[4], val[7], val[4]);
+                    if (edge & 256)
+                        vertexList[8] = vertexInterp(isolevel, p[0], p[4], val[0], val[4]);
+                    if (edge & 512)
+                        vertexList[9] = vertexInterp(isolevel, p[1], p[5], val[1], val[5]);
+                    if (edge & 1024)
+                        vertexList[10] = vertexInterp(isolevel, p[2], p[6], val[2], val[6]);
+                    if (edge & 2048)
+                        vertexList[11] = vertexInterp(isolevel, p[3], p[7], val[3], val[7]);
 
-					// Genero los triangulos
-					for (int i = 0; triTable[cubeIndex][i] != -1; i += 3) {
-						const Vertex& a = vertexList[triTable[cubeIndex][i]];
-						const Vertex& b = vertexList[triTable[cubeIndex][i + 1]];
-						const Vertex& c = vertexList[triTable[cubeIndex][i + 2]];
-						const Vertex normal = normalize(crossProduct(b - a, c - a));
+                    // Genero los triangulos
+                    for (int i = 0; triTable[cubeIndex][i] != -1; i += 3) {
+                        const Vertex& a = vertexList[triTable[cubeIndex][i]];
+                        const Vertex& b = vertexList[triTable[cubeIndex][i + 1]];
+                        const Vertex& c = vertexList[triTable[cubeIndex][i + 2]];
+                        const Vertex normal = normalize(crossProduct(b - a, c - a));
 
-						normArray[numVertex] = normal;
-						vertArray[numVertex++] = a;
-						normArray[numVertex] = normal;
-						vertArray[numVertex++] = b;
-						normArray[numVertex] = normal;
-						vertArray[numVertex++] = c;
-					}
-				}
-			}
-		}
+                        normArray[numVertex] = normal;
+                        vertArray[numVertex++] = a;
+                        normArray[numVertex] = normal;
+                        vertArray[numVertex++] = b;
+                        normArray[numVertex] = normal;
+                        vertArray[numVertex++] = c;
+                    }
+                }
+            }
+        }
 
-		return numVertex;
-	}
+        const float scale = 2.0f / (float)SIZE;
+        const Vertex offset = { 1.0f, 1.0f, 1.0f };
+        for (int i = 0; i < numVertex; i++) {
+            vertArray[i] = vertArray[i] * scale - offset;
+        }
+
+        return numVertex;
+    }
 }
 
 int main() {
-	Vertex* trianglesArray = (Vertex*)malloc(100 * 100 * 100 * 1000);
-	Vertex* normalsArray = (Vertex*)malloc(100 * 100 * 100 * 1000);
-	int numTriangles = generate_mesh(trianglesArray, normalsArray, 100, 0.5);
-	cout << "Numero de triangulos: " << numTriangles << endl;
+    Vertex* trianglesArray = (Vertex*)malloc(100 * 100 * 100 * 1000);
+    Vertex* normalsArray = (Vertex*)malloc(100 * 100 * 100 * 1000);
+    int numTriangles = generate_mesh(trianglesArray, normalsArray, 100, 0.5);
+    cout << "Numero de triangulos: " << numTriangles << endl;
 
-	return 0;
+    return 0;
 }
